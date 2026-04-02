2ad17c75f58c468eac5deaac57b491ddf4785138
Shevinperera <86422014+Shevinperera@users.noreply.github.com>
2026-04-02 15:21:41 +0530

    fixed idle conenction issue

--- config/db.js
+++ config/db.js
@@ -1,20 +1,24 @@
 const mysql = require('mysql2');
 const dotenv = require("dotenv");
 
-dotenv.config(); // loads .env from project root by default
+dotenv.config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
+db.getConnection((err, conn) => {
   if (err) {
     console.error("MySQL connection failed:", err);
   } else {
     console.log("MySQL connected...");
+    conn.release();
   }
 });
 
